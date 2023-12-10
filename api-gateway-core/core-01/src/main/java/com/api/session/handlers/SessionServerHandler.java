fbb0adfbd9e11f0148defc9bf620af76322682b0
qakqs <1248001568@qq.com>
2023-12-10 11:31:18 +0800

    1

--- api-gateway-core/core-01/src/main/java/com/api/session/handlers/SessionServerHandler.java
+++ api-gateway-core/core-01/src/main/java/com/api/session/handlers/SessionServerHandler.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.api.session.BaseHandler;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.*;
 import org.slf4j.Logger;
